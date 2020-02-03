7fadfa96fcb167a4c8213f1f4939f7e77a144004
Willis Koa <williskoa@yahoo.com>
2020-02-03 17:17:15 -0500

    Test added for fizzbuzz number divided by 15

--- 16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataNumbersToWords.java
+++ 16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataNumbersToWords.java
@@ -3,12 +3,22 @@ package com.techelevator;
 public class KataNumbersToWords {
 
 	// Attributes
+	char[] numberChars = new char[6];
 	String[] singleDigits = new String[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
 			"Nine" };
 	String[] tens = new String[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
 			"Seventeeen", "Eighteen", "Nineteen" };
 
+	String[] multipleTen = new String[] {"Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+	
 	public String convertNumberToWord(int num) {
+		
+		int numLength = numberChars.length;
+		
+		if (numLength > 1) {
+			return singleDigits[num];
+		}
+		
 		if (num>=10 && num<20) {
 			
 			return tens[num-10];
@@ -17,9 +27,9 @@ public class KataNumbersToWords {
 		if (num == 0) {
 			return singleDigits[0];
 		}
-		if (num > 0) {
-			return singleDigits[num];
-		}
+//		if (num > 0) {
+//			return singleDigits[num];
+//		}
 
 		return null;
 	}

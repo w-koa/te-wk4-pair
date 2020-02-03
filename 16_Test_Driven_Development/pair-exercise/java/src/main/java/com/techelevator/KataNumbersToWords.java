e20d27a9baaf29c38fd1da1a3017c1024ae7efd9
Willis Koa <williskoa@yahoo.com>
2020-02-03 14:11:09 -0500

    Int 1 returns one passed

--- 16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataNumbersToWords.java
+++ 16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataNumbersToWords.java
@@ -1,5 +1,21 @@
 package com.techelevator;
 
 public class KataNumbersToWords {
+	
+	// Attributes
+	String[] singleDigits = new String[] {"Zero", "One", "Two", "Three",
+											"Four", "Five", "Six", "Seven",
+											"Eight", "Nine"};
+	
+	public String convertNumberToWord(int num) {
+		if (num == 0) {
+			return singleDigits[0];
+		}
+		if (num > 0) {
+			return singleDigits[num + 1];
+		}
+		
+		return null;
+	}
 
 }

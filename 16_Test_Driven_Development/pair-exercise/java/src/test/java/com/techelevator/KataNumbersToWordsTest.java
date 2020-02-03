4199f57952b73b79b5167bfdaefedac0789f830e
Andorra Fields <andorrashek@yahoo.com>
2020-02-03 14:00:22 -0500

    Kata numbers to words test for 1 to return one

--- 16_Test_Driven_Development/pair-exercise/java/src/test/java/com/techelevator/KataNumbersToWordsTest.java
+++ 16_Test_Driven_Development/pair-exercise/java/src/test/java/com/techelevator/KataNumbersToWordsTest.java
@@ -7,4 +7,13 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class KataNumbersToWordsTest {
 
+	@Test
+	public void tests_that_1_returns_one() {
+
+		KataNumbersToWords numbers = new KataNumbersToWords();
+
+		String expected = "one";
+		String actual = numbers.convertNumberToWord(1);
+
+	}
 }

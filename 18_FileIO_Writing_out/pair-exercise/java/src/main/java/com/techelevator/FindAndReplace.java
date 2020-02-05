e690422ddfcc23447a2b8f993e6c2b4e40cff36a
Willis Koa <williskoa@yahoo.com>
2020-02-05 12:54:13 -0500

    Find and Replace initial commit

--- 18_FileIO_Writing_out/pair-exercise/java/src/main/java/com/techelevator/FindAndReplace.java
+++ 18_FileIO_Writing_out/pair-exercise/java/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,42 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter file path: ");
+		String filePath = input.nextLine();
+		
+		System.out.println("Enter a word to search for: ");
+		String wordInput = input.nextLine();
+		
+		System.out.println("Enter a word to replace " + wordInput + " with: ");
+		String replaceWord = input.nextLine();
+		
+		// for pasting: alices_adventures_in_wonderland.txt
+		File sourceFile = new File(filePath);
+		if (sourceFile.exists() == false) {
+			System.out.println("File does not exist");
+		} else if (sourceFile.isFile() == false) {
+			System.out.println("This is not a file");
+		}
+		
+		try (Scanner fileScanner = new Scanner(sourceFile)){
+			while(fileScanner.hasNextLine()) {
+				String words = fileScanner.nextLine();
+				if (words.contains(wordInput)) {
+					String replacedWords = words.replace(wordInput, replaceWord);
+					System.out.println(replacedWords.toString());
+				}
+			}
+		} 
+		
+		input.close();
 	}
 
 }
